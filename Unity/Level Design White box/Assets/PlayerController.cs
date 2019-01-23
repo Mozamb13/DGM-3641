7d0c0bc72a998831d7bc36a48fc5321335d3aa9c
Corbin Walker <cdog10123@gmail.com>
2019-01-23 15:19:55 -0700

    Git Hub Upload

---
+++ Unity/Level Design White box/Assets/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+    private string moveInputAxis = "Vertical";
+    private string turnInputAxis = "Horizontal";
+
+    public static Action<float> KeyAction;
+
+    public float rotationRate = 360;
+
+    public float moveSpeed = 2;
+
+
+    // Start is called before the first frame update
+    void Update()
+    {
+       float moveAxis = Input.GetAxis(moveInputAxis);
+       float turnAxis = Input.GetAxis(turnInputAxis);
+    
+
+        ApplyInput(moveAxis, turnAxis);
+
+    }
+
+    // Update is called once per frame
+    private void ApplyInput(float moveInput, float turnInput)
+    
+    {
+        Move(moveInput);
+        Turn(turnInput);
+    }
+    private void Move(float input)
+
+    {
+        transform.Translate(Vector3.forward * input * moveSpeed);
+    }
+    private void Turn(float input)
+
+    {
+        transform.Rotate(0, input * rotationRate * Time.deltaTime, 0);
+
+    }
+}
+
